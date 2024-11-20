e0c85f909df0d04fdf22a043bafa99b542b77a8a
Daniil Cherkesov <daniilerkesov122@gmail.com>
2024-11-20 13:52:37 +0300

    Реализация валидации формы

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./scss/main.scss";
+
+import IMask from "imask";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const phoneInput = document.querySelector("#tel");
+    const nameInput = document.querySelector("#name");
+
+    if (phoneInput) {
+        IMask(phoneInput, {
+            mask: "+{7} (000) 000-00-00",
+        });
+    }
+
+    if (nameInput) {
+        IMask(nameInput, {
+            mask: /^[A-Za-zА-Яа-яЁё\s]*$/,
+            lazy: false,
+        });
+    }
+});
